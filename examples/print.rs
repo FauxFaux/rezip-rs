dbe6ee017b1d7f36bea67ba518d2f6088a4e369a
Chris West (Faux) <git@goeswhere.com>
2017-09-21 12:58:03 +0100

    faster example

--- examples/print.rs
+++ examples/print.rs
@@ -2,12 +2,13 @@ extern crate librezip;
 
 use std::env;
 use std::fs;
+use std::io;
 
 fn main() {
     let input = env::args().nth(1).expect("first argument: input-path.gz");
     let output = env::args().nth(2).expect("second argument: output-path");
     let results = librezip::process(
-        fs::File::open(input).expect("input readable"),
+        io::BufReader::new(fs::File::open(input).expect("input readable")),
         fs::OpenOptions::new().write(true).create_new(true).open(output).expect("output creatable"),
     ).expect("processing");
 

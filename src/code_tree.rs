bf6275fcb71849aa8d3a324bddfce1fc8dd5450a
Chris West (Faux) <git@goeswhere.com>
2017-09-21 17:57:52 +0100

    don't even use box for the temporary lists

--- src/code_tree.rs
+++ src/code_tree.rs
@@ -16,19 +16,19 @@ impl CodeTree {
             "too many lengths"
         );
 
-        let mut nodes: Vec<Box<Node>> = Vec::with_capacity(16);
+        let mut nodes: Vec<Node> = Vec::new();
 
         let fifteen_to_zero_inclusive = (0..16).rev();
         for i in fifteen_to_zero_inclusive {
             ensure!(nodes.len() % 2 == 0, "not a tree");
 
-            let mut new_nodes: Vec<Box<Node>> = Vec::with_capacity(16);
+            let mut new_nodes: Vec<Node> = Vec::new();
 
             // add leaves for positive lengths
             if i > 0 {
                 for j in 0..canonical_code_lengths.len() {
                     if i == canonical_code_lengths[j] {
-                        new_nodes.push(Box::new(Node::Leaf(j as u32)));
+                        new_nodes.push(Node::Leaf(j as u32));
                     }
                 }
             }
@@ -40,10 +40,10 @@ impl CodeTree {
                     None => break,
                 };
 
-                new_nodes.push(Box::new(Node::Internal(
-                    first,
-                    iter.next().expect("list is even in length"),
-                )));
+                new_nodes.push(Node::Internal(
+                    Box::new(first),
+                    Box::new(iter.next().expect("list is even in length")),
+                ));
             }
 
             nodes = new_nodes;
@@ -51,7 +51,7 @@ impl CodeTree {
 
         ensure!(1 == nodes.len(), "non-canonical code");
 
-        match *nodes.into_iter().next().unwrap() {
+        match nodes.into_iter().next().unwrap() {
             Node::Internal(left, right) => Ok(CodeTree {
                 left: *left,
                 right: *right,

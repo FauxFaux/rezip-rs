833272f5dad7d7864c4b95ea9b3173dd56ca310b
Chris West (Faux) <git@goeswhere.com>
2017-09-22 11:33:55 +0100

    move gzip code out of lib.rs

---
+++ src/gzip.rs
@@ -0,0 +1,57 @@
+use std::io::Read;
+
+use errors::*;
+
+pub fn discard_header<R: Read>(mut from: R) -> Result<()> {
+    let mut header = [0u8; 10];
+    from.read_exact(&mut header)?;
+
+    ensure!(0x1f == header[0] && 0x8b == header[1], "invalid magic");
+    ensure!(0x08 == header[2], "unsupported compression method");
+
+    let flags = header[3];
+    ensure!(0 == (flags & 0b1110_0000), "reserved flags bits set");
+    // 4, 5, 6, 7: mtime
+    // 8: extra flags (compression level)
+    // 9: OS
+
+    if has_bit(flags, 2) {
+        // extra
+        let mut buf = [0u8; 2];
+        from.read_exact(&mut buf)?;
+        let extra_field_length = ((buf[1] as usize) << 8) | (buf[0] as usize);
+        from.read_exact(&mut vec![0u8; extra_field_length])?;
+    }
+
+    if has_bit(flags, 3) {
+        // fname
+        read_null_terminated(&mut from)?;
+    }
+
+    if has_bit(flags, 4) {
+        // comment
+        read_null_terminated(&mut from)?;
+    }
+
+    if has_bit(flags, 1) {
+        // CRC
+        from.read_exact(&mut [0u8; 2])?;
+    }
+
+    Ok(())
+}
+
+#[inline]
+fn has_bit(val: u8, bit: u8) -> bool {
+    (val & (1 << bit)) == (1 << bit)
+}
+
+fn read_null_terminated<R: Read>(mut from: R) -> Result<()> {
+    loop {
+        let mut buf = [0u8; 1];
+        from.read_exact(&mut buf)?;
+        if 0 == buf[0] {
+            return Ok(());
+        }
+    }
+}

dd825ffa7afdc96b144d77d1239f2c9ba081470e
Chris West (Faux) <git@goeswhere.com>
2017-12-01 12:10:35 +0000

    thinking about moving gzip to the new framework

--- src/emulate.rs
+++ src/emulate.rs
@@ -5,89 +5,6 @@ use Code;
 use Ref;
 use usize_from;
 
-pub fn gzip(preroll: &[u8], data: &[u8]) -> Result<Vec<Code>> {
-    let mut ret = Vec::with_capacity(data.len() / 258);
-    let guesser = RefGuesser::new(preroll, data);
-
-    // if there's no saved run:
-    //  * if there are no possible runs, emit a literal
-    //  * if there is a run, save it
-    // if there's a saved run:
-    //  * if our best run is longer, emit a literal for the saved run, and overwrite it
-    //  * if our best run is shorter (possibly zero), emit the saved run;
-    //       it's guaranteed to squash this byte
-
-    let mut saved: Option<Ref> = None;
-
-    let mut pos = 0usize;
-    while pos < data.len() {
-        let cursor = guesser.at(pos);
-        let best = match cursor.all_candidates() {
-            Some(candidates) => best(candidates.filter(move |r| usize_from(r.dist) != pos)),
-            None => break,
-        };
-
-        println!("{}: saved: {:?} best: {:?}", pos, saved, best);
-
-        match best {
-            Some(best) => match saved {
-                Some(saved_ref) if best.run() > saved_ref.run() => {
-                    // there's a new ref here, and it's better.
-                    // emit a literal for the saved ref, and save this instead.
-                    println!(" - better: literal {:?}, saving {:?}, +1", data[pos], best);
-                    ret.push(Code::Literal(data[pos - 1]));
-                    saved = Some(best);
-                    pos += 1;
-                }
-                Some(saved_ref) => {
-                    // the old ref was better than this ref, so emit it directly;
-                    // discarding these matches as they're bad
-                    println!(" - worse: taking {:?}, dropping, +run", saved_ref);
-                    ret.push(Code::Reference(saved_ref));
-                    saved = None;
-                    pos += usize_from(saved_ref.run() - 1);
-                }
-                None => {
-                    println!(" - just us: saving {:?}, +1", best);
-                    // nothing saved, so just save this run
-                    saved = Some(best);
-                    pos += 1;
-                }
-            },
-            None => match saved {
-                Some(saved_ref) => {
-                    // no run here, if we have a saved ref, it's the one
-                    println!(
-                        " - nothing here, using saved: {:?}, dropping, +run",
-                        saved_ref
-                    );
-                    ret.push(Code::Reference(saved_ref));
-                    saved = None;
-                    pos += usize_from(saved_ref.run()) - 1;
-                }
-                None => {
-                    println!(" - nothing here, nothing saved, lit: {:?}, +1", data[pos]);
-                    // no run saved, and no run found, can only be a literal
-                    ret.push(Code::Literal(data[pos]));
-                    pos += 1;
-                }
-            },
-        }
-    }
-
-    if let Some(saved_ref) = saved {
-        ret.push(Code::Reference(saved_ref));
-        pos += usize_from(saved_ref.run());
-    }
-
-    while pos < data.len() {
-        ret.push(Code::Literal(data[pos]));
-        pos += 1;
-    }
-
-    Ok(ret)
-}
-
 pub fn greedy(guesser: &RefGuesser, pos: usize) -> Vec<Code> {
     let here = guesser.at(pos);
     let candidates = here.all_candidates();
@@ -98,6 +15,38 @@ pub fn greedy(guesser: &RefGuesser, pos: usize) -> Vec<Code> {
         },
     ]
 }
+
+pub fn gzip(guesser: &RefGuesser, mut pos: usize) -> Vec<Code> {
+    let mut ret = Vec::with_capacity(3);
+
+    let cursor = guesser.at(pos);
+    let mut current = match cursor.all_candidates().and_then(|candidates| {
+        best(candidates.filter(move |r| usize_from(r.dist) != pos))
+    }) {
+        Some(start) => start,
+        None => return vec![cursor.current_literal()],
+    };
+
+    loop {
+        pos += 1;
+        let cursor = guesser.at(pos);
+        current = match cursor.all_candidates().and_then(|candidates| {
+            best(candidates.filter(move |r| usize_from(r.dist) != pos))
+        }) {
+            Some(new) if new.run() > current.run() => {
+                ret.push(cursor.current_literal());
+                new
+            }
+            None | Some(_) => {
+                ret.push(Code::Reference(current));
+                break;
+            }
+        };
+    }
+
+    ret
+}
+
 
 pub fn three_zip(guesser: &RefGuesser, pos: usize) -> Vec<Code> {
     let first = guesser.at(pos);
@@ -179,7 +128,6 @@ fn best<I: Iterator<Item = Ref>>(mut candidates: I) -> Option<Ref> {
 
 #[cfg(test)]
 mod tests {
-    use super::gzip;
     use Code;
     use Code::Literal as L;
     use Ref;
@@ -195,6 +143,10 @@ mod tests {
             Some(Ref::new(2, 5)),
             best(vec![Ref::new(1, 3), Ref::new(2, 5)].into_iter())
         );
+    }
+
+    fn gzip(_: &[u8], _: &[u8]) -> ::errors::Result<Vec<Code>> {
+        unimplemented!("TODO")
     }
 
     #[test]

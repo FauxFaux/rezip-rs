72cf2f6a3337c5b03b806024e1026242675658b4
Chris West (Faux) <git@goeswhere.com>
2017-12-06 12:53:17 +0000

    that cat code is duplicated

--- rezippers/src/cat.rs
+++ rezippers/src/cat.rs
@@ -1,9 +1,7 @@
 use std::io;
 use std::io::Read;
-use std::io::Write;
 
 use librezip;
-use librezip::Block;
 use librezip::CircularBuffer;
 
 use errors::*;
@@ -17,19 +15,7 @@ pub fn run<R: Read>(mut reader: R) -> Result<()> {
     let mut dictionary = CircularBuffer::new();
 
     for block in librezip::parse_deflate(&mut reader).into_iter() {
-        let block = block?;
-        use self::Block::*;
-        match block {
-            Uncompressed(data) => {
-                stdout.write_all(&data)?;
-                dictionary.extend(&data);
-            }
-            FixedHuffman(ref codes) | DynamicHuffman { ref codes, .. } => {
-                let mut decompressed: Vec<u8> = Vec::with_capacity(codes.len());
-                librezip::decompressed_block(&mut decompressed, &mut dictionary, &block)?;
-                stdout.write_all(&decompressed)?;
-            }
-        }
+        librezip::decompressed_block(&mut stdout, &mut dictionary, &block?)?;
     }
 
     Ok(())
